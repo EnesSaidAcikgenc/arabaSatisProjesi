8284bfb748f66e299512b1dea99b2672c538d8b5
EnesSaidAcikgenc <110100794+EnesSaidAcikgenc@users.noreply.github.com>
2025-02-17 15:36:15 +0300

    template eklentileri eklendi

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -8,4 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Car extends Model
 {
     use HasFactory;
+    protected $table = 'cars';
+
+    protected $fillable = ['user_id'
+        ,'model_id'
+        ,'damage_id'
+        ,'year'
+        ,'km'
+        ,'color'
+        ,'guarentee_status'
+        ,'vites_turu'
+        ,'fuel_turu'
+        ,'posting_date'
+        ,'status'
+        ,'fiyat'
+        ,'description'
+    ];
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
+            $table->tinyInteger('role')->comment('0-SuperAdmin, 1-Admin, 2-Satıcı, ');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
